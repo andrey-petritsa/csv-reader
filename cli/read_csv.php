d8bb6d2b63c993e5137141c7077f0c42bb1932c6
andrey-petritsa <andrey@petritsa.ru>
2021-09-28 17:00:58 +0300

    Учел возможность дубликатов

--- cli/read_csv.php
+++ cli/read_csv.php
@@ -6,13 +6,19 @@ use Csv\CsvReader;
 $PATH_TO_CSV = './employee.csv';
 
 $isReadingCsvHeaders = true;
+$employees = [];
 foreach (CsvReader::readLine($PATH_TO_CSV) as $csvLine) {
     if ($isReadingCsvHeaders) {
         $isReadingCsvHeaders = false;
         continue;
     }
 
-    $employee = CsvReader::parseCsvLineToEmployee($csvLine);
+    $employees[] = CsvReader::parseCsvLineToEmployee($csvLine);
+}
+
+$employees = array_unique($employees, SORT_REGULAR);
+
+foreach ($employees as $employee) {
     echo "Employee id: {$employee->getId()} Employee name: {$employee->getName()}, Employee hours: {$employee->getWorkedHours()}";
     echo PHP_EOL;
 }
