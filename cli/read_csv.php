47da5701f9e54121c2ea15f873ea99632f48f49e
andrey-petritsa <andrey@petritsa.ru>
2021-09-28 16:42:39 +0300

    Написал пример клиентского кода для чтения csv

--- cli/read_csv.php
+++ cli/read_csv.php
@@ -5,6 +5,14 @@ use Csv\CsvReader;
 
 $PATH_TO_CSV = './employee.csv';
 
+$isReadingCsvHeaders = true;
 foreach (CsvReader::readLine($PATH_TO_CSV) as $csvLine) {
-    var_dump($csvLine);
+    if ($isReadingCsvHeaders) {
+        $isReadingCsvHeaders = false;
+        continue;
+    }
+
+    $employee = CsvReader::parseCsvLineToEmployee($csvLine);
+    echo "Employee id: {$employee->getId()} Employee name: {$employee->getName()}, Employee hours: {$employee->getWorkedHours()}";
+    echo PHP_EOL;
 }
