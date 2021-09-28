80f13548b67ac9870982e138978ed67c8c6e5481
andrey-petritsa <andrey@petritsa.ru>
2021-09-28 15:57:44 +0300

    Вынес получения пути вывода файла в конструктор, добавил метод добавления заголовка к csv

--- src/csv/Fixture/FixtureGenerator.php
+++ src/csv/Fixture/FixtureGenerator.php
@@ -4,34 +4,43 @@ namespace Csv\Fixture;
 
 class FixtureGenerator
 {
-    const PATH_TO_SEETTINGS = './fixture_settings.json';
+    const PATH_TO_SEETTINGS = __DIR__ . '/./fixture_settings.json';
+
+    private string $outputCsvFilePath;
+    private array $csvHeaders;
 
     private int $maxCountOfLines;
     private array $nameOptions;
     private int $minWorkHours;
     private int $maxWorkHours;
 
-    private string $outputPath;
-
-
-    public function __construct()
+    public function __construct(string $outputCsvFilePath)
     {
+        $this->outputCsvFilePath = $outputCsvFilePath;
+
         $json = file_get_contents(self::PATH_TO_SEETTINGS);
         $settings = json_decode($json, true);
 
         $this->maxCountOfLines = $settings['maxCountOfLines'];
+
         $this->nameOptions = $settings['nameOptions'];
         $this->minWorkHours = $settings['workHoursOptions']['min'];
         $this->maxWorkHours = $settings['workHoursOptions']['max'];
 
-        $this->outputPath = $settings['outputPath'];
+        $this->csvHeaders = $settings['columnName'];
     }
 
     public function generateCsv()
     {
-        for ($i = 0; $i < $this->maxCountOfLines; $i++) {
+        if (file_exists($this->outputCsvFilePath)) {
+            unlink($this->outputCsvFilePath);
+        }
+
+        $this->writeCsvHeaders();
+
+        for ($i = 0; $i <= $this->maxCountOfLines; $i++) {
             $dummyEmployee = $this->getRandomEmployee();
-            file_put_contents($this->outputPath, (string)$dummyEmployee, FILE_APPEND | LOCK_EX);
+            file_put_contents($this->outputCsvFilePath, (string)$dummyEmployee . PHP_EOL, FILE_APPEND | LOCK_EX);
         }
     }
 
@@ -39,11 +48,14 @@ class FixtureGenerator
     {
         $dummyEmployee = new EmployeeDTO();
         $dummyEmployee->setId(rand(1, $this->maxCountOfLines));
-        $dummyEmployee->setName(array_rand($this->nameOptions));
+        $dummyEmployee->setName($this->nameOptions[array_rand($this->nameOptions)]);
         $dummyEmployee->setWorkedHours(rand($this->minWorkHours, $this->maxWorkHours));
 
         return $dummyEmployee;
     }
 
-
+    private function writeCsvHeaders()
+    {
+        file_put_contents($this->outputCsvFilePath, implode(",", $this->csvHeaders) . PHP_EOL);
+    }
 }
