74e57ea263bd4a7078e391408009eb4178035305
andrey-petritsa <andrey@petritsa.ru>
2021-09-30 09:33:37 +0300

    Добавил фасад для работы с модулем

---
+++ src/csv/Employee/EmployeeCsvReader.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Csv\Employee;
+
+use Csv\CsvReader;
+use Csv\Factory\EmployeeFactory\EmployeeCreator;
+use Csv\Employee\Validation\EmployeeValidator;
+use Csv\Factory\ErrorFactory\ErrorCreator;
+
+class EmployeeCsvReader
+{
+    private CsvReader $csvReader;
+    private EmployeeValidator $employeeValidator;
+    private EmployeeCreator $employeeCreator;
+    private EmployeeHelper $employeeHelper;
+
+    private array $employees;
+
+    public function __construct(string $pathToCsvFile)
+    {
+        $this->csvReader = new CsvReader($pathToCsvFile);
+        $this->employeeValidator = new EmployeeValidator(new ErrorCreator());
+        $this->employeeCreator = new EmployeeCreator($this->employeeValidator);
+        $this->employeeHelper = new EmployeeHelper();
+        $this->employees = [];
+    }
+
+    public function readUniqueEmployees(): array
+    {
+
+        foreach ($this->csvReader->readContentRow() as $csvRow) {
+            $employee = $this->employeeCreator->getEmployeeFromRow($csvRow);
+            $this->employees[] = $employee;
+        }
+
+        return $this->employeeHelper->getUniqueEmployees($this->employees);
+    }
+}
