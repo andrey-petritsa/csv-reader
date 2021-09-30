8209a3e30f87462d58395f60b2dc8c3779216993
andrey-petritsa <andrey@petritsa.ru>
2021-09-30 10:43:25 +0300

    Добавил фасад для генерации csv

---
+++ src/csv/Employee/EmployeeCsvGenerator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Csv\Employee;
+
+use Csv\CsvWriter;
+use Csv\Fixture\FixtureGenerator;
+use Csv\Fixture\FixtureSettings;
+
+class EmployeeCsvGenerator
+{
+    private FixtureGenerator $fixtureGenerator;
+
+    public function __construct($pathToFile)
+    {
+        $csvWriter = new CsvWriter($pathToFile);
+        $settings = new FixtureSettings();
+
+        $this->fixtureGenerator = new FixtureGenerator($settings, $csvWriter);
+    }
+
+    public function generateEmployees() {
+        $this->fixtureGenerator->generateCsvWithEmployees();
+    }
+}
